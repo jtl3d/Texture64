6fb39102f692f19ff6b0e557246b6168572348fe
John Liljegren <jtl3d@virginia.edu>
2019-10-23 21:56:02 -0400

    bat file accepts multiple files, name based on the original file name

--- Texture64.bat
+++ Texture64.bat
@@ -1,2 +1,6 @@
-java -jar "%~dp0Texture64.jar" %1
+FOR %%A IN (%*) DO (
+	java -jar "%~dp0Texture64.jar" %%A
+	move "%%~dpAout.rgba5551" "%%~dpnA.rgba5551"
+	move "%%~dpAout.rgba8888" "%%~dpnA.rgba8888"
+)
 pause
